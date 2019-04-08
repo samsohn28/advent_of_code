f46c31c0271eb73754cf5941573878cfff8c921c
samsohn28 <sam.sohn28@gmail.com>
2019-04-08 16:34:37 -0700

    Reorganizing files once again, completed solution for Day 2 Part 2.

---
+++ Day2/Part2/day2pt2_solution.py
@@ -0,0 +1,28 @@
+# We will sort the data and then go through it in 1 pass
+
+fp = open('input.txt', 'r')
+lines = fp.readlines()
+lines.sort()
+
+last = ''
+mismatch_count = 0
+mismatch_letter = ''
+
+for line in lines:
+    if last == '':
+        last = line
+    else:
+        for i in range (0,len(line)-1):
+            if last[i] != line[i]:
+                mismatch_count += 1
+                mismatch_letter = last[i]
+        if mismatch_count == 1:
+            print(last)
+            print(line)
+            break
+        else:
+            mismatch_count = 0
+            mismatch_letter = ''
+            last = line
+
+            
